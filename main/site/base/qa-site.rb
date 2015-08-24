c45e3762be303db2878a7bdd35cfb912de49828b
t-umiki <tumiki.git@gmail.com>
2015-08-25 00:45:44 +0900

    create yahoochiebukuro clowler

--- main/site/base/qa-site.rb
+++ main/site/base/qa-site.rb
@@ -1,1 +1,29 @@
 # _*_ coding: utf-8 _*_
+require 'nokogiri'
+require 'open-uri'
+require 'uri'
+
+require 'capybara'
+require 'capybara/dsl'
+require 'selenium-webdriver'
+
+class QASite
+  include Capybara::DSL
+
+  attr_reader :base_url
+  attr_reader :url
+  attr_reader :title
+  attr_reader :description
+  attr_reader :tags
+  attr_reader :update_date
+  attr_reader :view_count
+
+  def initialize()
+    raise "not implement exception. please override initialize()"
+  end
+
+  def run()
+    raise "not implement exception. please override run()"
+  end
+
+end
